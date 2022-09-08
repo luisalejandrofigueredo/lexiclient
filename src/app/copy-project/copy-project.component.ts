d463577d9c5734d9b5f906e12a2289748c4fbe73
luisalejandrofigueredo <luisalejandrofigueredo@gmail.com>
2022-09-07 21:47:45 -0300

    07/09/2022

--- src/app/copy-project/copy-project.component.ts
+++ src/app/copy-project/copy-project.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Project } from '../interfaces/project';
 import { ProjectService } from "../project.service";
@@ -13,24 +14,33 @@ export class CopyProjectComponent implements OnInit {
     name: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
     description: new FormControl<string>('', { nonNullable: true }),
   });
-  id!:string;
-  constructor(private projectService:ProjectService,private route: ActivatedRoute,private router:Router) { }
+  id!: string;
+  oldName!: string;
+  constructor(private matSnackBar: MatSnackBar, private projectService: ProjectService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-     this.route.params.subscribe((params)=>{
-      this.id=params['id'];
-       this.projectService.getOne(this.id).then((resolve)=>{
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+      this.projectService.getOne(this.id).then((resolve) => {
         if (typeof resolve !== 'boolean') {
           this.formProject.controls.name.setValue(resolve.projectName);
           this.formProject.controls.description.setValue(resolve.description);
+          this.oldName = resolve.projectName;
         }
-       });
-     });
+      });
+    });
+    this.formProject.controls.name.valueChanges.subscribe((value) => {
+      if (this.oldName === value) {
+        this.matSnackBar.open('Duplicate project name', 'Error', { duration: 3000 });
+        this.formProject.controls.name.markAsPristine();
+      };
+    })
   }
 
-  submit() { }
+  submit() {
+  }
 
-  cancel() { 
+  cancel() {
     this.router.navigate(['viewProjects']);
   }
 
